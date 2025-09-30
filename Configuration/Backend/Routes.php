ff23f835b76048f6f0ead56c9d9532aafb7d0455
thomas-sc <info@schefter.net>
2025-09-30 14:20:46 +0200

    Clean up, remove unused services, commands, JavaScript, and configurations.

--- Configuration/Backend/Routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'tx_liszt_catalog_raisonne' => [
-        'path' => '/liszt/catalograisonne/works',
-        'target' => 'Slub\\LisztCatalograisonne\\Classes\\Controller\\WorkController::index',
-    ],
-];
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,37 +2,16 @@
 
 declare(strict_types=1);
 
-use Slub\LisztCatalograisonne\Controller\WorkController;
-use Slub\LisztCatalograisonne\Services\MermeidXmlRpcService;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Log\LogLevel;
 
 defined('TYPO3') or die();
 
-ExtensionUtility::configurePlugin(
-    'LisztCatalograisonne',
-    'WorkListing',
-    [ WorkController::class => 'index' ],
-    [ WorkController::class => 'index' ]
-);
+$extConf = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('liszt_catalograisonne');
 
-ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:liszt_catalograisonne/Configuration/TsConfig/Page/Mod/Wizards/Listing.tsconfig">'
-);
-
-ExtensionManagementUtility::addService(
-    'LisztCatalograisonne',
-    'mermeid',
-    'tx_lisztcatalograisonne_mermeid',
-    [
-        'title' => 'MerMEId',
-        'description' => 'Retrieve and store documents in a MerMEId instance',
-        'subtype' => '',
-        'available' => true,
-        'priority' => 50,
-        'quality' => 50,
-        'os' => '',
-        'exec' => '',
-        'className' => MermeidXmlRpcService::class
-    ]
-);
+$logLevel = match((int)($extConf['logLevel'] ?? 3)) {
+    0 => LogLevel::INFO,
+    1 => LogLevel::NOTICE,
+    2 => LogLevel::WARNING,
+    default => LogLevel::ERROR
+};
